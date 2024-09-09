6c947629b4122454336fc77e5c27268ce9f2ef54
amorim26 <nathalywerner262@gmail.com>
2024-09-09 20:20:35 -0300

    AULA 09/09

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-   
+$(document).ready(function() {
+    var nr_c = "";
+
+    $('.bt-calc').click(function(){
+        var nr = $(this).text();
+        nr_c = nr_c.concat(nr)
+
+        $('#txt_display').text(nr_c)
+    })
 });
