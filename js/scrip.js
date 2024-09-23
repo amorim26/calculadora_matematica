8c918469a50aa5e80599dc5700c7bc9e91a167cd
amorim26 <nathalywerner262@gmail.com>
2024-09-23 20:15:29 -0300

    23/09

--- js/scrip.js
+++ js/scrip.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
+
+    // $('#txt_display').mask('000\'000\'000\'000\'000', {reverse: true});
+
     var bt_nr = '';
 
-    $('.bt-calc').click(function() {
-        var n = $(this).text()
+    $('.bt-nr').click(function() {
+        var n = $(this).text();
 
-        if (n == '0' || nr == '00') {
-            alert('1')
-            if (bt_nr.length > 0) {
+        if( n == '0' || n == '00' ) {
+
+            if( bt_nr.length > 0 ) {
+
                 bt_nr = bt_nr.concat(n);
+
             }
+
         } else {
+
             bt_nr = bt_nr.concat(n);
+
         }
 
-        bt_nr = bt_nr.concat(n);
         $('#txt_display').val(bt_nr);
-    })
+    });
+
 });
